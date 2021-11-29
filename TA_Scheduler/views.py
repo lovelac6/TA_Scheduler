ffa1bdb43bc8f007afbcee1af098fe504f6f87c9
nalovelace <lovelac1@uwm.edu>
2021-11-29 11:52:33 -0600

    Added login.html and some setup for it.

--- TA_Scheduler/views.py
+++ TA_Scheduler/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+
 
 # Create your views here.
+class Login(View):
+    def get(self, request):
+        return render(request, "login.html", {})
+
+    def post(self, request):
+        return render(request, "login.html", {})
