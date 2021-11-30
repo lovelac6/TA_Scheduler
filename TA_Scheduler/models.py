562a75d14e76f3a9b61455298c19ff8309a10ea9
ecger <ecgervasi16@gmail.com>
2021-11-30 12:07:51 -0600

    Separated user and account type in database. Added homeView.html Added login test methods.

--- TA_Scheduler/models.py
+++ TA_Scheduler/models.py
@@ -9,5 +9,8 @@ class UserType(models.TextChoices):
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+
+class AccountType(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     accountType = models.CharField(max_length=1, choices=UserType.choices, default="T")
 
--- TA_Scheduler/tests/tests.py
+++ TA_Scheduler/tests/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from TA_Scheduler.models import User
 # Create your tests here.
+class Login(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(username="existingUser", password="secret")
+
+    def test_loginGoesToHomeView(self):
+        response = self.client.post("/", {"username": "existingUser", "password": "secret"})
+        self.assertEquals(response.url.__str__(), '/homeView/',
+                          msg="Logging in does not navigate user to user's home page")
+
+    def test_wrongPasssword(self):
+        response = self.client.post("/", {"username": "existingUser", "password": "wrongPassword"}, follow=True)
+        self.assertEqual(response.context["message"], "bad password", msg="Wrong password message not displayed with incorrect password")
+
+    def test_noPassword(self):
+        response = self.client.post("/", {"username": "existingUser", "password": ""}, follow=True)
+        self.assertEqual(response.context["message"], "bad password",
+                         msg="No bad password message when user does not enter a password")
+
+    def test_wrongUsername(self):
+        response = self.client.post("/", {"wrongUsername": "", "password": "secret"}, follow=True)
+        self.assertEqual(response.context["message"], "bad username",
+                         msg="No bad username message when user does not enter a password")
+
+    def test_noUsername(self):
+        response = self.client.post("/", {"username": "", "password": "secret"}, follow=True)
+        self.assertEqual(response.context["message"], "bad username",
+                         msg="No bad username message when user does not enter a password")
+
+    def test_logout(self):
+        self.client.post("/", {"username": "existingUser", "password": "secret"}, follow=True)
+        response = self.client.get("/", {}, follow=True)
+        with self.assertRaises(KeyError, msg="Logged in user did not successfully logout"):
+            response.context["username"]
+
+    def test_switchUsers(self):
+        self.client.post("/", {"username": "existingUser", "password": "secret"}, follow=True)
+        self.client.get("/", {}, follow=True)
+        response = self.client.post("/", {"username": "newUser", "password": "secret"}, follow=True)
+        self.assertEquals(response.context["username"], "newUser", msg="Switching users was unsuccessful")
+
