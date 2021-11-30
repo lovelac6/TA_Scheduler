417cf1849931270c9c23d78e0d2bce5626d71812
kino174 <70708859+kino174@users.noreply.github.com>
2021-11-30 11:47:29 -0600

    Update views.py
    using previous lab as a model, changed things up a bit to match what we want.

--- TA_Scheduler/views.py
+++ TA_Scheduler/views.py
@@ -8,4 +8,14 @@ class Login(View):
         return render(request, "login.html", {})
 
     def post(self, request):
-        return render(request, "login.html", {})
+        noSuchUser = False
+        badPassword = False
+        try:
+            m = MyUser.objects.get(name=request.POST['name'])
+            badPassword = (m.password != request.POST['password'])
+        except:
+            noSuchUser = True
+        if noSuchUser | badPassword:
+            return render(request, "login.html", {"message": "invalid login"})
+        else:
+            return redirect("") #Still Deciding on what will be post-login page
