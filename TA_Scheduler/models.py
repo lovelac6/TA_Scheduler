0a09e532f63cada07035556e4ba8420e702e01ba
ecger <ecgervasi16@gmail.com>
2021-12-02 14:59:37 -0600

    Added Course and CourseAssignment tables to models

--- TA_Scheduler/models.py
+++ TA_Scheduler/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
 class UserType(models.TextChoices):
     Supervisor = "S"
     Instructor = "I"
     TA = "T"
 
+
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
-    accountType = models.CharField(max_length=1, choices=UserType.choices, default="T")
+    accountType = models.CharField(max_length=1, choices=UserType.choices, default=UserType.TA)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=20)
+    number = models.IntegerField()
+
+
+class CourseAssignment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
 
