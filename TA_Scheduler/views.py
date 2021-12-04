652efd870c848164218c7a691d350875b5cd16c5
ecger <ecgervasi16@gmail.com>
2021-12-04 15:50:29 -0600

    created side navigation bar and added skeleton courses and user html pages

--- TA_Scheduler/views.py
+++ TA_Scheduler/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from TA_Scheduler.models import User
 
+
 # Create your views here.
 class Login(View):
     def get(self, request):
@@ -12,14 +13,26 @@ class Login(View):
         badPassword = False
         try:
             m = User.objects.get(username=request.POST['username'])
+            print(m)
             badPassword = (m.password != request.POST['password'])
         except:
             noSuchUser = True
         if noSuchUser | badPassword:
             return render(request, "login.html", {"message": "invalid login"})
         else:
-            return redirect("/home/") #Still Deciding on what will be post-login page
+            return redirect("/home/")  # Still Deciding on what will be post-login page
+
 
 class Home(View):
     def get(self, request):
         return render(request, "homeView.html", {})
+
+
+class Courses(View):
+    def get(self, request):
+        return render(request, "courses.html", {})
+
+
+class Users(View):
+    def get(self, request):
+        return render(request, "users.html", {})
